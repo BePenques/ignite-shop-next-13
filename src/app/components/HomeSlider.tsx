15c71a25538d6cd8b30ab5730bccf6ec4cc6dd5a
Betiza Barreira <be.penques@hotmail.com>
2024-10-29 18:52:05 -0300

    Static Site Generation SSG

--- src/app/components/HomeSlider.tsx
+++ src/app/components/HomeSlider.tsx
@@ -1,13 +1,7 @@
 'use client'
 import { HomeContainer, Product } from "@/styles/pages/home";
 import Image from 'next/image'
-
-import camiseta1 from '../../assets/shirts/1.png'
-import camiseta2 from '../../assets/shirts/2.png'
-import camiseta3 from '../../assets/shirts/3.png'
-
 import { useKeenSlider } from "keen-slider/react";
-
 
 interface HomeSliderProps {
   products:{
@@ -27,21 +21,24 @@ export default function HomeSlider({products}:HomeSliderProps) {
     }
   })
 
-return (
-        <HomeContainer ref={sliderRef} className="keen-slider">
-        {
-            products.map(product=>{
-                return (
-                <Product key={product.id} className="keen-slider__slide">
-                    <Image src={product.imageUrl} width={520} height={480} alt=""/>
-                    <footer>
-                    <strong>{product.name}</strong>
-                    <span>{product.price}</span>
-                    </footer>
-                </Product>
-                )
-            })
-        }
-        </HomeContainer>
+return (    
+      <HomeContainer ref={sliderRef} className="keen-slider">
+      {
+        products && 
+        (
+          products.map(product=>{
+            return (
+                  <Product key={product.id} className="keen-slider__slide">
+                      <Image src={product.imageUrl} width={520} height={480} alt=""/>
+                      <footer>
+                      <strong>{product.name}</strong>
+                      <span>{product.price}</span>
+                      </footer>
+                  </Product>
+            )
+          })
+        )
+      }
+      </HomeContainer>   
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,45 @@ import 'keen-slider/keen-slider.min.css'
 import { stripe } from "@/lib/stripe";
 import Stripe from "stripe";
 import HomeSlider from "./components/HomeSlider";
+import { GetStaticProps } from 'next';
 
+interface Product {
+  id: string;
+  name: string;
+  imageUrl: string;
+  price: number | null;
+}
 
-export default async function Home() {
+interface HomeProps {
+  products: Product[];
+}
 
-  const productsList = await stripe.products.list({
-        expand: ['data.default_price']
-      })
-
-  const products =  productsList.data.map(product=>{
-        const price = product.default_price as Stripe.Price
-        return {
-          id: product.id,
-          name: product.name,
-          imageUrl: product.images[0],
-          price: price?.unit_amount ? price.unit_amount / 100 : null
-        }
-      })
+export default async function Home({ products }: HomeProps) {
 
   return (
     <HomeSlider products={products}/>
   );
 }
+
+export const getStaticProps: GetStaticProps = async()=>{//Static Site Generation SSG - npm run build
+  
+  const productsList = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+
+  const products = productsList.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price?.unit_amount ? price.unit_amount / 100 : null,
+    };
+  });
+
+  return {
+    props: { products }, // Passa os produtos como props para a página
+  };
+}
+
 
