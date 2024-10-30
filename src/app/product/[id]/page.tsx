091d6caa4ae3a884092ccc47a4eeead7ca9a02cd
Betiza Barreira <be.penques@hotmail.com>
2024-10-30 15:44:51 -0300

    estrutura da pagina de produtos

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product";
+
 interface Props {
     params: {id: string}
 }
@@ -6,9 +8,16 @@ export default function Product({params}: Props) {
 
 
   return (
-    <div>
-      <h1>Detalhes do Produto</h1>
-      <p>ID do produto: {params.id}</p>
-    </div>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis libero cumque dignissimos vero eius laudantium repudiandae nam doloremque ipsam maiores labore minus, iure animi ex ipsum earum, ducimus voluptatum saepe.</p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
